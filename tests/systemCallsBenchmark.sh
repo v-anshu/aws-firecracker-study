0690a48e38df360c8451aa79864112fcb07ffcfd
Arun Jose <arun123jose@gmail.com>
2019-11-09 12:24:43 -0600

    Test to benchmark SystemCalls time

---
+++ tests/systemCallsBenchmark.sh
@@ -0,0 +1,4 @@
+max=1000                                                                                                                              
+for i in `seq 2 $max`                                                                                                                 do                                                                                                                                        
+	date +%H:%M:%S >/dev/null                                                                                                                  
+done
