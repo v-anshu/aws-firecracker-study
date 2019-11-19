199685db309f1686164b844797cffa09c4749eea
calvincodes <arpitjain1821@gmail.com>
2019-11-18 18:27:38 -0600

    Created dirs for FC and gVisor. Renamed scripts

---
+++ gVisor/scripts/startup_time_benchmark.sh
@@ -0,0 +1,3 @@
+docker run --name MyContainer1 --runtime=runsc --rm -it -d alpine
+docker run --name MyContainer2 --runtime=runsc --rm -it -d alpine
+docker run --name MyContainer3 --runtime=runsc --rm -it -d alpine
