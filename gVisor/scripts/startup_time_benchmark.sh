b92f29b02cd0c5bb458b41c72eecd53b66b2f565
calvincodes <arpitjain1821@gmail.com>
2019-11-21 20:27:38 -0600

    Adding gVisor startup benchmark script

--- gVisor/scripts/startup_time_benchmark.sh
+++ gVisor/scripts/startup_time_benchmark.sh
@@ -1,3 +1,20 @@
-docker run --name MyContainer1 --runtime=runsc --rm -it -d alpine
-docker run --name MyContainer2 --runtime=runsc --rm -it -d alpine
-docker run --name MyContainer3 --runtime=runsc --rm -it -d alpine
+#!/bin/bash
+
+# Single Threaded Run
+# time parallel bash startup_time_benchmark.sh ::: 1234 > /dev/null 2>&1
+# Multi Threaded Run
+# time parallel bash startup_time_benchmark.sh ::: 1001 2101 3201 4301 5401 6501 7601 8701 9801 10901 > /dev/null 2>&1
+# Non Parallel Run
+# time bash startup_time_benchmark.sh 1 > /dev/null 2>&1
+
+start=$1
+end=$start+10
+for ((i=$start; i<$end; i++)); do
+  docker run --name MyContainer-$i --runtime=runsc --rm -it -d alpine
+done
+
+for ((i=$start; i<$end; i++)); do
+  docker stop MyContainer-$i
+done
+
+docker stop $(docker ps -a -q)
