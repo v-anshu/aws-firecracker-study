fc7c03712e5e1b03a37009767e2fced29efed839
calvincodes <arpitjain1821@gmail.com>
2019-11-24 12:25:53 -0600

    Adding bash network script

---
+++ gVisor/scripts/network_benchmark.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# time sh ./network_benchmark.sh
+
+wget -O /dev/null http://90.130.70.73/1MB.zip > /dev/null 2>&1
+wget -O /dev/null http://90.130.70.73/1MB.zip > /dev/null 2>&1
+wget -O /dev/null http://90.130.70.73/1MB.zip > /dev/null 2>&1
+wget -O /dev/null http://90.130.70.73/1MB.zip > /dev/null 2>&1
+wget -O /dev/null http://90.130.70.73/1MB.zip > /dev/null 2>&1
+wget -O /dev/null http://90.130.70.73/1MB.zip > /dev/null 2>&1
+wget -O /dev/null http://90.130.70.73/1MB.zip > /dev/null 2>&1
+wget -O /dev/null http://90.130.70.73/1MB.zip > /dev/null 2>&1
+wget -O /dev/null http://90.130.70.73/1MB.zip > /dev/null 2>&1
+wget -O /dev/null http://90.130.70.73/1MB.zip > /dev/null 2>&1
