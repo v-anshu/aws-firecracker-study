c02685482a98d9327ac7a03733ddac685c9adce1
v-anshu <anshu.iiita.10@gmail.com>
2019-11-25 19:52:40 -0600

    memory_allocation

---
+++ c_scripts/memory-allocation/run_malloc_test.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+make clean
+make malloc_free
+./malloc_free 100000 1 > malloc_free_1KB.txt
+./malloc_free 100000 2 > malloc_free_2KB.txt
+./malloc_free 100000 4 > malloc_free_4KB.txt
+./malloc_free 100000 8 > malloc_free_8KB.txt
+./malloc_free 100000 512 > malloc_free_512KB.txt
+./malloc_free 100000 1024 > malloc_free_1MB.txt
+make clean
+make malloc_without_free
+./malloc_without_free 100000 1 > malloc_free_1KB.txt
+./malloc_without_free 100000 2 > malloc_free_2KB.txt
+./malloc_without_free 100000 4 > malloc_free_4KB.txt
+./malloc_without_free 100000 8 > malloc_free_8KB.txt
+./malloc_without_free 100000 512 > malloc_free_512KB.txt
+./malloc_without_free 100000 1024 > malloc_free_1MB.txt
