342119d3af7f30c63f2ddd76e4409790138a4ef8
v-anshu <anshu.iiita.10@gmail.com>
2019-11-25 20:17:20 -0600

    Slight changes

--- c_scripts/memory-allocation/run_malloc_test.sh
+++ c_scripts/memory-allocation/run_malloc_test.sh
@@ -1,29 +1,15 @@
 #!/usr/bin/env bash
-make clean
 make malloc_free
 ./malloc_free 100000 1 > malloc_free_1KB.txt
-echo "Malloc with free 1KB done..."
 ./malloc_free 100000 2 > malloc_free_2KB.txt
-echo "Malloc with free 2KB done..."
 ./malloc_free 100000 4 > malloc_free_4KB.txt
-echo "Malloc with free 4KB done..."
 ./malloc_free 100000 8 > malloc_free_8KB.txt
-echo "Malloc with free 8KB done..."
 ./malloc_free 100000 512 > malloc_free_512KB.txt
-echo "Malloc with free 512KB done..."
 ./malloc_free 100000 1024 > malloc_free_1MB.txt
-echo "Malloc with free 1MB done..."
-make clean
 make malloc_without_free
-./malloc_without_free 100000 1 > malloc_free_1KB.txt
-echo "Malloc without free 1KB done..."
-./malloc_without_free 100000 2 > malloc_free_2KB.txt
-echo "Malloc without free 2KB done..."
-./malloc_without_free 100000 4 > malloc_free_4KB.txt
-echo "Malloc without free 4KB done..."
-./malloc_without_free 100000 8 > malloc_free_8KB.txt
-echo "Malloc without free 8KB done..."
-./malloc_without_free 100000 512 > malloc_free_512KB.txt
-echo "Malloc without free 512KB done..."
-./malloc_without_free 100000 1024 > malloc_free_1MB.txt
-echo "Malloc without free 1MB done..."
+./malloc_without_free 100000 1 > malloc_without_free_1KB.txt
+./malloc_without_free 100000 2 > malloc_without_free_2KB.txt
+./malloc_without_free 100000 4 > malloc_without_free_4KB.txt
+./malloc_without_free 100000 8 > malloc_without_free_8KB.txt
+./malloc_without_free 100000 512 > malloc_without_free_512KB.txt
+./malloc_without_free 100000 1024 > malloc_without_free_1MB.txt
