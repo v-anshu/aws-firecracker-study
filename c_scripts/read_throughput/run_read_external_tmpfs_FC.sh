56fdbe3a83813caccfac79da8917e66bfd827a96
jose-arun <arun123jose@gmail.com>
2019-11-30 04:08:40 -0600

    read throughput changes

---
+++ c_scripts/read_throughput/run_read_external_tmpfs_FC.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+make read
+mkdir results
+
+echo "Reading a file from disk for 100k iterations, read size = 4KB"
+rm -f ./results/results_read_ext_tmpfs_4KB.txt
+touch ./results/results_read_ext_tmpfs_4KB.txt
+./read  100000 4000 /mnt/file >> ./results/results_read_ext_tmpfs_4KB.txt
+
+echo "Reading a file from disk for 100k iterations, read size = 16KB"
+rm -f ./results/results_read_ext_tmpfs_16KB.txt
+touch ./results/results_read_ext_tmpfs_16KB.txt
+./read  100000 16000 /mnt/file >> ./results/results_read_ext_tmpfs_16KB.txt
+
+echo "Reading a file from disk for 100k iterations, read size = 64KB"
+rm -f ./results/results_read_ext_tmpfs_64KB.txt
+touch ./results/results_read_ext_tmpfs_64KB.txt
+./read  100000 64000 /mnt/file >> ./results/results_read_ext_tmpfs_64KB.txt
+
+echo "Reading a file from disk for 100k iterations, read size = 256KB"
+rm -f ./results/results_read_ext_tmpfs_256KB.txt
+touch ./results/results_read_ext_tmpfs_256KB.txt
+./read  100000 256000 /mnt/file >> ./results/results_read_ext_tmpfs_256KB.txt
+
+echo "Reading a file from disk for 100k iterations, read size = 1MB"
+rm -f ./results/results_read_ext_tmpfs_1MB.txt
+touch ./results/results_read_ext_tmpfs_1MB.txt
+./read  100000 1000000 /mnt/file >> ./results/results_read_ext_tmpfs_1MB.txt
+
+make clean
